eb1a4ca85bb96ca4e5fa6faec73def101b4f8887
Ahmad <myvirtualrabbit15@gmail.com>
2021-01-08 08:35:28 +0600

    #14

---
+++ src/animation.js
@@ -0,0 +1,15 @@
+export const pageAnimation = {
+  hidden: {
+    opacity: 0,
+    x: -300,
+    y: -300,
+  },
+  show: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
